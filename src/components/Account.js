f1f0a7489b40017793d28fd01035155bd8b1b77e
Rod512 <rudro149187@bsdi-bd.org>
2023-06-30 02:11:28 +0600

    project almost done

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Fragment } from 'react';
+import { Fragment,useState } from 'react';
 import image from '../img/profile.png'
 
 const Account = () => {
     const profilename = JSON.parse(localStorage.getItem('user_name'))
+    const [deposite,setDeposite] = useState('')
+    const [withdraw,setWithdraw] = useState('')
+    const deposite_money = JSON.parse(localStorage.getItem('deposite'))
+    const withdraw_money = JSON.parse(localStorage.getItem('withdraw'))
+
+    const depo = (e)=>{
+        e.preventDefault()
+        localStorage.setItem('deposite',JSON.stringify(deposite))
+    }
+
+    const with_d = (e)=>{
+        e.preventDefault()
+        localStorage.setItem('withdraw',JSON.stringify(withdraw))
+
+    }
+
+    
     return (
         <Fragment>
             <div className='my-2 mx-5 md:mx-0 md:my-0 lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 '>
@@ -23,14 +40,38 @@ const Account = () => {
 
                      <div className='border py-10 bg-rose-500 font-bold text-white rounded-md'>
                         <h3>Last Withdraw</h3>
-                        <p>20$</p>
+                        <p><span dangerouslySetInnerHTML={{__html:withdraw_money}}></span>$</p>
                      </div>
 
                      <div className='border py-10 bg-lime-500 font-bold text-white rounded-md'>
                         <h3>Last Deposite</h3>
-                        <p>10$</p>
+                        <p><span dangerouslySetInnerHTML={{__html:deposite_money}}></span> $</p>
                      </div>
                 </div>
+                <div className='mt-32 flex justify-center'>
+                    <form class="w-full max-w-sm" onChange={depo}>
+                    <p className='text-4xl pb-20 text-center'>Create Your Banking here!</p>
+                        <div class="flex items-center border-b border-teal-500 py-2">
+                            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder={profilename} aria-label="Full name" onChange={(e)=>setDeposite(e.target.value)}/>
+                                <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                                    Deposite
+                                </button>
+                        </div>
+                    </form>
+                </div>
+
+                <div className='mt-12 flex justify-center'>
+                    <form class="w-full max-w-sm" onChange={with_d}>
+                        <div class="flex items-center border-b border-teal-500 py-2">
+                            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder={profilename} aria-label="Full name" onChange={(e)=>setWithdraw(e.target.value)}/>
+                                <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                                    Withdraw
+                                </button>
+                        </div>
+                    </form>
+                </div>
+
+
             </div>
             
         </Fragment>
